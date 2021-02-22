f2632864410f9dd77caa12e0b79084d29e8877a6
Phoebe <phoebe.wilckens@gmail.com>
2021-02-22 14:38:55 -0300

    update the syntaxis of the array

--- memory.js
+++ memory.js
@@ -1,5 +1,5 @@
 /*Variables*/
-let cards = new Array(
+let cards = [
   { name: "1", selection: false },
   { name: "2", selection: false },
   { name: "3", selection: false },
@@ -16,7 +16,7 @@ let cards = new Array(
   { name: "6", selection: false },
   { name: "7", selection: false },
   { name: "8", selection: false }
-);
+];
 let tries = 0;
 let play1 = (play2 = "");
 let idJ1 = (idJ2 = "");
